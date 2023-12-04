082c090c5e50807b2456d44b17fa67ea4189babb
D4FALT <152661953+D4FALT@users.noreply.github.com>
2023-12-03 22:59:03 -0300

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,63 @@
+var dev = "https://github.com/D4FALT";
+var twitter = "https://twitter.com";
+var linkedin = "https://www.linkedin.com";
+var instagram = "https://instagram.com/_pedrovisk_ofc_?utm_source=qr&igshid=OGU0MmVlOWVjOQ==";
+var github = "https://github.com/D4FALT";
+var tetris = "https://jstris.jezevec10.com";
+var email = "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=pedroluc4sdebarros@gmail.com";
+
+sobremim = [
+  "<br>",
+  "<span class='underline'>Ola, meu nome é Pedro Lucas de Barros</span>",
+  "<br>",
+  "<li>📚Ensino Médio completo - 2023 - Nair Ferreiras Neves.</li>",
+  "<li>👨🏻‍💻 Cursos Adicionais:</li>",
+  `<pre class="whitespace-pre-wrap">
+        - HTML
+        - Java
+        - Photoshop
+        - CSS
+        - Python
+        - Dreamweaver</pre>`,
+  `<li><pre class="whitespace-pre-wrap">🗂️ Venho me colocar-me a disposição destra empresa para exercer as funções. 
+  Espero vir ser útil, preciso de uma oportunidade de trabalho para adquirir experiência profissional e pessoal, coloco-me a disposição para entrevistas e testes que sejam necessários. 
+  Busco oportunidade como Auxiliar de Informática para aplicar meus conhecimentos técnicos, habilidades analíticas e comprometimento, contribuindo para o desenvolvimento e eficiência do setor de tecnologia. 
+  Estudei Programação em 2021 e trabalhei por alguns meses na area de informática`,
+
+  `<li>📫 Entre em contato: <a href= "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=pedroluc4sdebarros@gmail.com">pedroluc4sdebarros@gmail.com</a>`,
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="blank">instagram' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="blank">github.com/D4FALT' +
+    "</a>",
+  
+  "<br>",
+];
+ajuda = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">sobremim</span>    
+   <span class="command">↳</span>&nbsp;Quem eu sou?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lista de redes sociais.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;Para me contactar usando email.
+  <span class="command">historico</span>    
+   <span class="command">↳</span>&nbsp;Veja o histórico de comandos.
+  <span class="command">ajuda</span>       
+   <span class="command">↳</span>&nbsp;Mostre a lista de comandos.
+  <span class="command">tetris</span>       
+   <span class="command">↳</span>&nbsp;Se queiser jogar um tetris comigo é só me chamar.
+  <span class="command">limpe</span>
+   <span class="command">↳</span>&nbsp;Limpe o terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Seja bem vindo ao meu portifolio! — Digite <span class="command text-[#75e1e7]">ajuda</span> Para acessar a lista de comandos.</span></div>`,
+];
---
+++ js/main.js
@@ -0,0 +1,165 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer");
+var textarea = document.getElementById("texter");
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+var commands = [];
+
+setTimeout(function () {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+ 
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine(
+        "[D4FALT]~$" + command.innerHTML,
+        "no-animation",
+        0
+      );
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "ajuda":
+      loopLines(ajuda, "color2 margin", 80);
+      break;
+    case "sobremim":
+      loopLines(sobremim, "color2 margin", 80);
+      break;
+
+    case "DEV":
+      addLine("Opening Dev.to...", "color2", 80);
+      newTab(Dev);
+      break;
+
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "historico":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine(
+        'Abrindo email:<a href="mailto:pedroluc4sdebarros@gmail.com"> pedroluc4sdebarros@gmail.com</a>...',
+        "color2",
+        80
+      );
+      newTab(email);
+      break;
+      newTab(email);
+      break;
+    case "limpe":
+      setTimeout(function () {
+        terminal.innerHTML =
+          '<a id="before"><div class="pt-2"><span class="text-[#7d82d7db] ">Seja bem vindo ao meu portifolio! — Digite <span class="command text-[#75e1e7]">ajuda</span> Para acessar a lista de comandos.</span></div></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    // socials
+    case "dev":
+      addLine("Opening Dev.to...", "color2", 80);
+      newTab(dev);
+      break;
+    case "twitter":
+      addLine("Opening Twitter...", "color2", 0);
+      newTab(twitter);
+      break;
+    case "linkedin":
+      addLine("Opening LinkedIn...", "color2", 0);
+      newTab(linkedin);
+      break;
+    case "instagram":
+      addLine("Opening Instagram...", "color2", 0);
+      newTab(instagram);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    case "tetris":
+      addLine("Tente fazer uma boa pontuação...", "color2", 0);
+      newTab(tetris);
+      break;
+    default:
+      addLine(
+        '<span class="inherit">Erro: Comando não encontrado. Para acessar a lista de comandos, d <span class="command">\'ajuda\'</span>.</span>',
+        "error",
+        100
+      );
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function () {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function () {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function (item, index) {
+    addLine(item, style, index * time);
+  });
+}
